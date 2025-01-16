20e3fafeb0c5309f4c44fcfa3c0aeb1941f7d0d1
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2025-01-16 15:41:51 +0700

    Home Layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+
+const Home = () => (
+    <>
+        <BookOverview />
+        <BookList />
+    </>
+);
+export default Home;
