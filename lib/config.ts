4c28453608710f3363e47ff3fb2b553de7b52821
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2025-01-22 14:31:43 +0700

    User onboarding flow

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
     env: {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+        proApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
         imageKit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
@@ -8,8 +9,10 @@ const config = {
         },
         databaseUrl: process.env.DATABASE_URL!,
         upstash: {
-            redisUrl: process.env.UPSTASH_REDIS_URL,
-            redisToken: process.env.UPSTASH_REDIS_TOKEN,
+            redisUrl: process.env.UPSTASH_REDIS_URL!,
+            redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+            qStashUrl: process.env.QSTASH_URL!,
+            qStashToken: process.env.QSTASH_TOKEN!,
         },
     },
 };
